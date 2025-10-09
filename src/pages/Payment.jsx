b60846ac2c7fee1c9066cc202783d7f814fdb2ec
xivamjha <jhashivam1383@gmail.com>
2025-10-09 15:46:17 +0530

    upi changed to harshit

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -422,7 +422,7 @@ const PaymentMethods = ({ finalPrice, onCancel, onPaymentError, onEmailSubmit })
 
   const generateUpiLink = (amount) => {
     const upiParams = {
-      pa: 'shinescreeningmovies@axl',
+      pa: '9871342190@jio',
       pn: 'Shine Screenings',
       am: amount,
       cu: 'INR',
