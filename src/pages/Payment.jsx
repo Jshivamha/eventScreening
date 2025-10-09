4c9a4b129d32d8c2df62f3af0eacbe6214ae0724
xivamjha <jhashivam1383@gmail.com>
2025-10-09 15:42:14 +0530

    upi changed

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -422,7 +422,7 @@ const PaymentMethods = ({ finalPrice, onCancel, onPaymentError, onEmailSubmit })
 
   const generateUpiLink = (amount) => {
     const upiParams = {
-      pa: 'shinescreening@ybl',
+      pa: 'shinescreeningmovies@axl',
       pn: 'Shine Screenings',
       am: amount,
       cu: 'INR',
