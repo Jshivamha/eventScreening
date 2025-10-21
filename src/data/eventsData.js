37ce01a8f1f9f27033a3d426cb287869a90ad64a
xivamjha <jhashivam1383@gmail.com>
2025-10-21 10:38:27 +0530

    time

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -35,7 +35,7 @@ export const eventsData = [
     description: "Exclusive private screening of Interstellar. Perfect for corporate events, birthdays, or special celebrations. Customize your experience with us.",
     image: "https://miro.medium.com/0*NNP_zZwHt9Zu3Bzz",
     price: "699",
-    capacity: "50 people",
+    capacity: "18 seats left",
     city: "Delhi NCR"
   },
   {
